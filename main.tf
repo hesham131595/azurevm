aa14ce3f1a379a181e40b11ad16620e53bcf1e9a
hesham131595 <93712347+hesham131595@users.noreply.github.com>
2022-11-19 16:46:55 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -80,7 +80,7 @@ resource "azurerm_linux_virtual_machine" "linuxvm" {
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.nic.id]
   size                  = "Standard_DS1_v2"
-
+  custom_data = base64encode(file("scripts/init.sh"))
   os_disk {
     name                 = "myOsDisk"
     caching              = "ReadWrite"
